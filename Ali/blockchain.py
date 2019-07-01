eca53261e8b4af59f0375cfb36c6928d41ed0128
Ali <Muhammad.Ayub.Eng@gmail.com>
2019-07-01 19:40:45 -0400

    first commit

---
+++ Ali/blockchain.py
@@ -0,0 +1,95 @@
+## https://github.com/satwikkansal/python_blockchain_app/blob/3d252de03586ebb96acb689842ca2d451c0eec47/node_server.py
+## https://freestartupkits.com/articles/technology/cryptocurrency-news-and-tips/how-to-build-a-blockchain-application-python/ 
+## conda install -c anaconda flask
+
+from hashlib import sha256
+import json
+import time
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+    def __init__(self):
+        self.unconfirmed_transactions = [] # data yet to get into blockchain
+        self.chain = []
+        self.create_genesis_block()
+    def create_genesis_block(self):
+        """
+        A function to generate genesis block and appends it to
+        the chain. The block has index 0, previous_hash as 0, and
+        a valid hash.
+        """
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+    @property
+    def last_block(self):
+        return self.chain[-1]
+    
+    # difficulty of PoW algorithm
+    difficulty = 2
+    """
+    Previous code contd..
+    """
+    def proof_of_work(self, block):
+        """
+        Function that tries different values of nonce to get a hash
+        that satisfies our difficulty criteria.
+        """
+        block.nonce = 0
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+        return computed_hash
+    
+    """
+    Previous code contd..
+    """
+    def add_block(self, block, proof):
+        """
+        A function that adds the block to the chain after verification.
+        """
+        previous_hash = self.last_block.hash
+        if previous_hash != block.previous_hash:
+            return False
+        if not self.is_valid_proof(block, proof):
+            return False
+        block.hash = proof
+        self.chain.append(block)
+        return True
+    def is_valid_proof(self, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+    def mine(self):
+        """
+        This function serves as an interface to add the pending
+        transactions to the blockchain by adding them to the block
+        and figuring out Proof of Work.
+        """
+        if not self.unconfirmed_transactions:
+            return False
+        last_block = self.last_block
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
+                          previous_hash=last_block.hash)
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
---
+++ README.md
@@ -0,0 +1,2 @@
+### Hi! This is our Research Project that Validates and Builds Macaroon's Paper By Google: https://ai.google/research/pubs/pub41892
+

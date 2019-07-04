f8151bf926f63a08456a3f428fdba5658d839859
Ali <Muhammad.Ayub.Eng@gmail.com>
2019-07-03 23:50:48 -0400

    hey guys, I have added some code for the createMacaroons and addCaveatHelper - please be sure to copy into your files

--- Brandon/macaroons_lib.py
+++ Brandon/macaroons_lib.py
@@ -1,25 +1,56 @@
+import hmac
+import hashlib
+import base64
+import time
 
+"""
+key: encryption key
+id: random_nonce / payload
+"""
+def CreateMacaroon(key, id, location):
+    data = hmac.new(key, id, hashlib.sha256)
+    signature_str = data.hexdigest()  # KLUDGE: can we go back and forth from hexdigest()
+    macaroon_obj = Macaroon( id , [], signature_str)
+    macaroon_obj.targetLocation = location 
+    return macaroon_obj
 
 class Macaroon(object):
-    def __init__(self):
-        print("hello world")
+    def __init__(self, id, caveatsList, signature):
+        self.caveats = caveatsList
+        self.id = id
+        self.sig = signature 
+        #### 
+        self.targetLocation = None
+        self.thirdPartyLocations = [] 
 
-    def createMacaroon(self):
-        pass
-
-    def addCaveatHelper(self):
-        pass
+    def addCaveatHelper(self, cId, vId, caveat_location):
+        ### KLUDGE: "pattern matching" in the addCaveatHelper
+        caveat = {'cid': cId, 'vid': vId, 'clocation':caveat_location }
+        sig_prime =  hmac.new(self.sig, caveat['vid']+caveat['cid'] , hashlib.sha256)
+        self.caveats.append(caveat)
+        self.sig = sig_prime
+        return self  
 
     def addThirdPartyCaveat(self):
+        vId = 
         pass
     
-    def addFirstPartyCaveat(self):
-        pass
+    def addFirstPartyCaveat(self, a):
+        self.addCaveatHelper(a, 0, self.targetLocation )
     
     def prepareForRequest(self):
         pass
     
     def verify(self):
         pass 
-    
 
+
+
+##
+#
+#
+#
+#
+#
+#
+##
--- Patrick/test_lib.py
+++ Patrick/test_lib.py
@@ -1,4 +1,30 @@
 
-from macaroons_lib import Macaroon
+import macaroons_lib
 
-a = Macaroon()
+
+"""
+    This function tests... 
+"""
+def test1_CreateMacaroon():
+    #### Input: data 
+    id = ""
+    key = ""
+    location = ""
+    #### Output:  compute hmac on the outside 
+    hmac_value = hmac.new(key, id, hashlib.sha256)
+    #### use library to compute HMAC
+    M = macaroons_lib.CreateMacaroon(key, id, location)
+    #### Assertion: Does the library's output equal the expected value
+    assert(M.sig == hmac_value)
+    assert(M.id == id)
+    
+def test2_CreateMacaroon():
+    id = ""
+    key = ""
+    location = ""
+    macaroon = macaroons_lib.createMacaroon()
+
+
+
+if(__name__ == "__main__"):
+    test1_CreateMacaroon()

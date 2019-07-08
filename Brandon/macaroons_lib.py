1cfaab911704f33dba83e147fd7d071d2cacc443
Ali <Muhammad.Ayub.Eng@gmail.com>
2019-07-07 21:10:29 -0400

    adding code for other folders as well

--- Brandon/macaroons_lib.py
+++ Brandon/macaroons_lib.py
@@ -2,6 +2,7 @@ import hmac
 import hashlib
 import base64
 import time
+from Crypto.Cipher import AES
 
 """
 key: encryption key
@@ -13,6 +14,42 @@ def CreateMacaroon(key, id, location):
     macaroon_obj = Macaroon( id , [], signature_str)
     macaroon_obj.targetLocation = location 
     return macaroon_obj
+
+
+def ENC(sig, key):
+    password = "12324211231"
+    key = hashlib.sha256(password).digest() ## output is 16 bytes
+    key = key[:16]
+    IV = 16 * '\x00'           # Initialization vector: discussed later
+    mode = AES.MODE_CBC
+    encryptor = AES.new(key, mode, IV=IV)
+    forEncryption = hashlib.sha256(str(sig) + str(key)).digest() 
+    ciphertext = encryptor.encrypt(forEncryption)
+    return 
+
+
+#KTS = dictionaryOfKeys[macaroon.id]
+#verify(myMacaroon, KTS)
+
+"""
+    Not the original "verify" in paper. 
+    This method only assumes that the Macaroon was created with first party caveats. 
+"""
+def verify(macaroon, K_TargetService ):
+    #### verify the K_TargetService with Random_Nonce
+    data = hmac.new(K_TargetService, macaroon.id, hashlib.sha256)
+    signature_str = data.hexdigest() 
+    #### verify the caveats 
+    for caveat in macaroon.caveats:
+        cId = caveat['cid']
+        vId = caveat['vid']
+        sig_prime =  hmac.new(signature_str, caveat['vid']+caveat['cid'] , hashlib.sha256)
+        signature_str = sig_prime.hexdigest()
+    
+    if(signature_str != macaroon.sig):
+        return false #### incorrect 
+    else: 
+        return true #### verified to be correct 
 
 class Macaroon(object):
     def __init__(self, id, caveatsList, signature):
@@ -28,12 +65,13 @@ class Macaroon(object):
         caveat = {'cid': cId, 'vid': vId, 'clocation':caveat_location }
         sig_prime =  hmac.new(self.sig, caveat['vid']+caveat['cid'] , hashlib.sha256)
         self.caveats.append(caveat)
-        self.sig = sig_prime
+        self.sig = sig_prime.hexdigest()
         return self  
 
-    def addThirdPartyCaveat(self):
-        vId = 
-        pass
+    def addThirdPartyCaveat(self, cK, cId, cL):
+        vId = ENC(self.sig, cK)
+        self.thirdPartyLocations.append(cL)
+        self.addCaveatHelper(cId, vId, cL)
     
     def addFirstPartyCaveat(self, a):
         self.addCaveatHelper(a, 0, self.targetLocation )
@@ -41,8 +79,8 @@ class Macaroon(object):
     def prepareForRequest(self):
         pass
     
-    def verify(self):
-        pass 
+
+        
 
 
 
--- Patrick/test_lib.py
+++ Patrick/test_lib.py
@@ -1,21 +1,53 @@
 
 import macaroons_lib
+import hmac
+import hashlib
+import base64
+import time
+from Crypto.Cipher import AES
 
+
+
+
+def testVerifyOfFirstPartyCaveats():
+    K_TargetService1 = "this is the secret key "
+    K_TargetService2 = "this is also the secret key "
+    random_nonce = str(433242342)
+    location = "Catonsville, 21228"
+
+    caveat1 = "level of coolness == high"
+    caveat2 = "champions == USA Women's Team"
+
+    M = macaroons_lib.CreateMacaroon(K_TargetService1, random_nonce, location)
+    M.addFirstPartyCaveat(caveat1)
+    M.addFirstPartyCaveat(caveat2)
+
+    receivedMacaroon = M
+
+    # M2 = macaroons_lib.CreateMacaroon(K_TargetService1, random_nonce, location)
+    # M2.addFirstPartyCaveat(caveat1)
+    # M2.addFirstPartyCaveat(caveat2)
+
+    assert(macaroons_lib.verify(receivedMacaroon, K_TargetService2 ) == False)
+    assert(macaroons_lib.verify(receivedMacaroon, K_TargetService1 ) == True)
 
 """
     This function tests... 
 """
 def test1_CreateMacaroon():
     #### Input: data 
-    id = ""
-    key = ""
-    location = ""
+    id = "abc"
+    key = "234324"
+    location = "DC"
     #### Output:  compute hmac on the outside 
     hmac_value = hmac.new(key, id, hashlib.sha256)
+    hmac_value_digest = hmac_value.hexdigest()
     #### use library to compute HMAC
     M = macaroons_lib.CreateMacaroon(key, id, location)
     #### Assertion: Does the library's output equal the expected value
-    assert(M.sig == hmac_value)
+    print(M.sig)
+    print(hmac_value_digest)
+    assert(M.sig == hmac_value_digest)
     assert(M.id == id)
     
 def test2_CreateMacaroon():
@@ -28,3 +60,4 @@ def test2_CreateMacaroon():
 
 if(__name__ == "__main__"):
     test1_CreateMacaroon()
+    testVerify()
